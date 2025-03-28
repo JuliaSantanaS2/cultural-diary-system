d7215d029e73efb30778286d9d7c494659ebe689
JuliaSantanaS2 <juliasantana.5834@gmail.com>
2025-03-28 09:32:30 -0300

    feat(Show):Create Class Show

--- src/Module/Show.java
+++ src/Module/Show.java
@@ -1,13 +1,27 @@
-/* package Module;
+package Module;
+
+import java.util.List;
 
 public class Show extends AudioVisualMedia {
+    private String yearConclusion ;
+    private int yearEnd;
 
-    private int yearConclusion;
-    private int seasonYear;
-    private int seasonEpisode;
+    public Show(String originalTitle, String gender, int yearRelease, int seasonYear, int seasonEpisode, List<String> whereWatch,String yearConclusion, int yearEnd){
+        super(originalTitle, gender, yearRelease, seasonYear, seasonEpisode, originalTitle, whereWatch);
+        this.yearConclusion = yearConclusion;
+        this.yearEnd = yearEnd;
+    }
 
-    public Show(String originalTittle, int yearRelease, int yearConclusion, int seasonYear, int seasonEpisode) {}
+    public String getStatus(){return yearConclusion;}
+    public void setStatus(String yearConclusion){
+        this.yearConclusion = yearConclusion;
+    }
 
+    public int getStars(){
+        return yearEnd;
+    }
+    public void setStars(int yearEnd){
+        this.yearEnd = yearEnd;
+    }
 
 }
-*/
